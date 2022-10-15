be9098ef0acb9aa756fccd80e370b50e73509629
aladinzecic <aladinzecic10@gmail.com>
2022-10-15 19:13:23 +0200

    push

---
+++ 2.cas/app.js
@@ -0,0 +1,44 @@
+//JS moze prikazati podatke na nekoliko nacina 
+
+//1. innerHTML
+document.getElementById("p").innerHTML ="paragraf"
+
+//2. write()
+
+document.write()//najcesci nacin za testiranje nekog koda
+
+//3.alert()
+alert("zdravo")
+
+//4. console.log() ispisivanje u konzoli
+
+console.log("poruka za ispis")
+
+//5.pravila nazivanj promenljivih 
+
+//promenljiva mora poceti sa nekom od sledecih znakova
+//1. slovo (veliko ili malo) A-Z,a-z
+//2. $ znak moze biti pocetni karakter neke promenljive
+//3._(moze i donja crta)
+
+//broj moze biti sadrzan ali promenljiva ne sme pocinjati brojem burazere sta ja kriv 
+
+//prom1= "neki string"
+//2prom= "x" // ne more 
+//prosek ocena = 4.14 //nema razmake 
+prosekOcena= 4.14 //burazere , preporoucljiv nacin zapisivanja promeljive 
+prosek_ocena = 4.14 //korektno
+ProsekOcena= 4.14 //korektno 
+
+// Postoje 4 nacina za deklarisanje neke promeljive:
+// 1. prom = 12 - dodeljivanje vrednosti bez da smo prethodno izvrsili deklaraciju 
+// 2. Koriscenjem var keyword (rezervisane reci) -
+// var je koriscena na starijim browserima (sve se manje koristi) 
+// var a = 5 
+
+// 3. koriscenjem let keyword  (rezervisana rec) 
+// let se najcesce koristi kada postoji sansa da se vrednost date promeljive menja 
+
+// 4. koriscenjem const keyword (rezervisane reci) 
+// const se najcesce koristi za definisanje konstanti koje nece menjati vrednost
+
