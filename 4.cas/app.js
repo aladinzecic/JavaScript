8f1e302d4a6affe5fc3c88a9222e3bd82de7467c
aladinzecic <aladinzecic10@gmail.com>
2022-10-25 23:52:31 +0200

    push

--- 4.cas/app.js
+++ 4.cas/app.js
@@ -1,7 +1,28 @@
-console.log(2**3%3)
-//2
-w=2;
-w+=5
-console.log(w)
-console.log("postovanje")
-//7
+// INTERAKCIJA SA KORISNIKOM //
+
+// prompt() metoda nam sluzi za komunikaciju sa korisnikom.
+
+const ime = prompt("Unesite vase ime: ");
+console.log(ime);
+
+// Sve sto korisnik unese je tipa string.
+const godine = prompt("Unesite svoju starost: ");
+console.log(godine);
+console.log(typeof godine);
+// Sad je neophodno godine prevesti u broj:
+
+// Postoje 2 nacina za prevodjenje stringa u broj:
+
+// 1.  Number(promenljiva)
+const brGodina = Number(godine);
+console.log(brGodina);
+console.log(typeof brGodina);
+
+// 2. +promenljiva
+const brGodina2 = +godine;
+console.log(brGodina2);
+console.log(typeof brGodina2);
+
+const visina = +prompt("Unesite svoju visinu u cm: ");
+console.log(visina);
+console.log(typeof visina);
