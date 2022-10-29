048b00366f09b18df656e423c83f94173a2cbee0
aladinzecic <aladinzecic10@gmail.com>
2022-10-29 13:56:22 +0200

    push

--- 6.cas/app.js
+++ 6.cas/app.js
@@ -31,8 +31,32 @@
 // }
 
 //suma neparnih prirodnih brojeva od 10 do 20
-let x=0;
-for(let i=11;i<=20;i+=2){
-    console.log(x=x+i)
-}
+// let x=0;
+// for(let i=11;i<=20;i+=2){
+//     console.log(x=x+i)
+// }
+
+
+// let x=+prompt("racun:");
+//     console.log(x);
+//     if(x>=50 && x<300){
+//         const y=x/10;
+//         console.log("baksis:"+x/10);
+//         console.log("ukupan racun:"+y+x)
+//     }
+//     else if(x>=300){
+//         const y=x/5;
+//         console.log("baksis:"+x/5);
+//         console.log("ukupan racun:"+y+x);
+//     }
+
+
+    let x=+prompt("racun:");
+    console.log("racun bez baksisa"+' '+x);
+    let z=x/10; let g=x+z;
+    let d=x/5; let f=x+d;
+    x>=50 && x<300
+    ? 
+    console.log("baksis je:"+' '+z+' '+"ukupan racun je:"+' '+g) 
+    :console.log("baksis je:"+' '+d+' '+"ukupan racun je:"+' '+f)
 
