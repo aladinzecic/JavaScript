df8ab55e6417de0d19eb8b58a1babf07b3cdcb52
aladinzecic <aladinzecic10@gmail.com>
2022-11-01 20:57:52 +0100

    push

---
+++ 8.cas/app.js
@@ -0,0 +1,38 @@
+//prebrojati i sabrati brojeve deljive sa 5 u interavlu od 1 do n
+// let n=+prompt("unesi n:");
+
+// let ukupno=0;
+// let suma=0
+// for(let i=1;i<=n;i++){
+//     if(i%5===0){
+//         ukupno++;
+//         suma=suma+i;
+//     }
+// }
+// console.log(ukupno);
+// console.log(suma)
+
+
+//iteracija od broja 99 do -99 i da se ispise zbir brojeva deljivih sa 4 i broja 14
+// for(let i=99;i>=-99;i--){
+//     if(i%4===0){
+//         console.log(i+14)
+//     }
+// }
+
+// let suma=0;
+// let brojac=0
+// //aritmeticka sredina brojeva koji su deljivi sa 3 iteracija ide od 3 do 17 uklucujuci
+// for(let i=3;i<=17;i++){
+//     if(i%3===0){
+//         brojac++;
+//         suma=suma+i //suma+=i
+
+//     }
+// }
+// console.log(suma/brojac);
+
+
+//
+
+
