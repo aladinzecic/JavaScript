2e07d967d21646b84ac8a3e5d1f9252348f0790e
aladinzecic <aladinzecic10@gmail.com>
2022-11-12 14:15:14 +0100

    push

--- 10.cas/app.js
+++ 10.cas/app.js
@@ -76,15 +76,71 @@
 // console.log(brojac)
 
 
-let brojac=0
-let recenica =prompt("unesi neku recenicu")
-for(let i=0;i<recenica.length;i++){
-    if(!isNaN(recenica[i])){
-        brojac++
-    }
-}
-console.log(brojac)
+// let brojac=0
+// let recenica =prompt("unesi neku recenicu")
+// for(let i=0;i<recenica.length;i++){
+//     if(!isNaN(recenica[i])){
+//         brojac++
+//     }
+// }
+// console.log(brojac)
 //uzvicnik je metoda da se okrene boolean ako je true bice false
+
+
+
+//unesi broj ispisati njegov kvadrat
+
+// let a=+prompt("unesi broj")
+
+// if(isNaN(a)){
+//     console.log("lose unet broj")
+// }
+
+
+//napisati program koji ispisuje u konzoli brojeve od 1 do 100 ukljucujuci sto tako da 
+//1. ako je broj deljiv sa tri treba ispisati fizz umesto tog broja ako je deljiv sa 5 ispisati buzz ako je deljiv i 
+//sa tri i sa 5 treba ispisati fizz buzz
+
+// for(let i=1;i<=100;i++){
+//     if(i%3===0){
+//         console.log("fizz")
+//     }
+//     else if(i%5===0){
+//         console.log("buzz")
+//     }
+//     else if(i%3===0&&i%5===0){
+//         console.log("fizz buzz")
+//     }
+//     else console.log(i)
+// }
+
+
+
+
+// let recenica="nakon danasnjeg testa cemo znati ko ce nastaviti sa drugim ciklusom it campa "
+// let recenica1=" "
+// console.log(recenica.length)
+// for (i=0;i<recenica.length;i++){
+//     if(recenica[i-4]==="a"&&recenica[i-5]==="c")
+//         recenica1+="."
+//     else if(recenica[i]==="a"||recenica[i]==="e"||recenica[i]==="i"||recenica[i]==="o"||recenica[i]==="u")
+//     recenica1+=  recenica[i].toUpperCase() 
+    
+    
+//         else if(1) recenica1+=  recenica[i]
+        
+//     }
+//     console.log(recenica1)
+
+console.log(3>2>1)
+
+
+
+
+
+
+
+
 
 
 
--- 11.cas/app.js
+++ 11.cas/app.js
@@ -1,22 +1,22 @@
-//napraviti novu recenicu samo bez razmaka
-// let recenica="Omare jesi li spremio test"
-// let novaRecenica=""
-// let brojac=0
-// for(let i=0;i<recenica.length;i++){
-//     if(recenica[i]===" "){
-//         brojac++
-// }
-// }
-// console.log(brojac)
-let recenica=prompt("unesi recenicu")
-let novarecenica=""
+// napraviti novu recenicu samo bez razmaka
+let recenica="Omare jesi li spremio test"
+let novaRecenica=""
+let brojac=0
 for(let i=0;i<recenica.length;i++){
-    if(recenica[i]=="a"&&recenica[i+1]=="n"){
-        novarecenica+="d"
-    }
-    else if(recenica[i]=="a"){
-        novarecenica+="t"
-    }
-    else novarecenica+=recenica[i]
+    if(recenica[i]===" "){
+        brojac++
 }
-console.log(novarecenica)
+}
+console.log(brojac)
+// let recenica=prompt("unesi recenicu")
+// let novarecenica=""
+// for(let i=0;i<recenica.length;i++){
+//     if(recenica[i]=="a"&&recenica[i+1]=="n"){
+//         novarecenica+="d"
+//     }
+//     else if(recenica[i]=="a"){
+//         novarecenica+="t"
+//     }
+//     else novarecenica+=recenica[i]
+// }
+// console.log(novarecenica)
