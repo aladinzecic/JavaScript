490a9da3f480937f89a677a46fadd3acbdd0884c
aladinzecic <aladinzecic10@gmail.com>
2022-11-24 20:07:02 +0100

    push

--- 12.cas/app.js
+++ 12.cas/app.js
@@ -55,3 +55,65 @@ function povrsina(a,b){
 }
 console.log(povrsina(1,3))
 
+//treci sa testa
+let recenica;
+recenica1 = "Nakon danasnjeg testa cemo znati ko ce nastaviti sa drugim ciklusom it campa"
+
+function nova(recenica){
+    let recenica2=" ";
+    for(let i=0;i<=recenica.length;i++){
+        if(i===recenica.length){
+recenica2+="."
+        }
+        else if(recenica[i]==="a"||recenica[i]==="u"||recenica[i]==="o"||recenica[i]==="i"||recenica[i]==="e"){
+            recenica2+=recenica[i].toUpperCase()
+        }
+        else {recenica2+=recenica[i]}
+    }
+    return recenica2;
+
+}
+console.log(nova(recenica1))
+
+
+//2. arrow function:
+// const myFunction=()=>{
+    
+// // }
+
+
+// const proizvod=(br1,br2)=>{
+//     return br1*br2
+// }
+// console.log(proizvod)
+// //ako arrow funkcija ima samo jedan parametar nije neophodno okruziti ga zagradama 
+// //ako je funckija jednostvna tj ako odmah vraca neki jednostavan izraz onda sama sintaksa izgleda 
+// //kraca(nije neophodno koristiti viticaste zagrade i return keyword)
+// const kvbroja=broj=> broj**2
+// let broj1;
+// const broj=()=>{
+//     broj1=+prompt("unesi broj")
+//     if(isNaN(broj1)){
+//         return("nije broj")
+//     }
+//     else if(broj1>0){
+//         return ("pozitivan")
+//     }
+//     else if(broj1<0){
+//         return ("negativan")
+//     }
+//     else if(broj1===0){
+//         return ("nula") 9 17
+
+//     }
+// }
+// console.log(broj(broj1))
+const satnica=(par1,par2)=>{
+    if(par1>=9&&par1<17&&par2>=0&&par2<=59){
+        return ("na vreme")
+    }
+    else return ("nije")
+}
+console.log(satnica(10,5))
+
+
