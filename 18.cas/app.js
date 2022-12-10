3baccfbc0dbc37620934cc456fb8d7e8c3993f7f
aladinzecic <aladinzecic10@gmail.com>
2022-12-10 18:56:25 +0100

    push

--- 18.cas/app.js
+++ 18.cas/app.js
@@ -75,24 +75,64 @@
 
 //je li recenica ana voli milovana il jok
 
-function ana(recenica){
-    let brojac=1
-    let i
-    let nova=" "
-    for(i=0;i<recenica.length;i++){
-        if(recenica[i]===" "){
-            continue
+// function ana(recenica){
+//     let brojac=1
+//     let i
+//     let nova=" "
+//     for(i=0;i<recenica.length;i++){
+//         if(recenica[i]===" "){
+//             continue
+//         }
+//         else nova+=recenica[i].toLowerCase()
+//     }
+//     let duzina=nova.length
+//     for(i=0;i<duzina/2;i++){
+//         if(nova[i]===nova[duzina-brojac]){
+//             brojac++
+//         }
+//     }
+//     if(brojac===duzina/2) return "respect"
+//         else return "nije"
+    
+// }
+// console.log(ana("ana VOlI milovana"))
+
+
+// function rec(recenica){
+//     let indeks1=recenica.indexOf(" ");
+//     let indeks2=recenica.lastIndexOf(" ");
+//     let result = recenica.slice(0, indeks1)
+//     let result1 = recenica.slice(indeks2+1)
+//     const recenica2=result.concat(" ",result1)
+    
+//     return recenica2
+// }
+// console.log(rec("ja idem ja idem"))
+
+
+// function rec(par1,par2,par3){
+//     let recenica2=""
+//     let result1 = par1.substr(par2,par3)
+    
+//     return result1
+// }
+// console.log(rec("ja idem ja idem",2,5))
+
+
+function rec(recenica,slovo){
+    let b=""
+    for(let i=0;i<recenica.length;i++){
+        if(recenica[i]!==slovo){
+            b+=recenica[i]
         }
-        else nova+=recenica[i].toLowerCase()
+        
     }
-    let duzina=nova.length
-    for(i=0;i<duzina/2;i++){
-        if(nova[i]===nova[duzina-brojac]){
-            brojac++
-        }
-    }
-    if(brojac===duzina/2) return "respect"
-        else return "nije"
+    return b
+
     
 }
-console.log(ana("ana VOlI milovana"))
+console.log(rec("ja idem ja idem","a"))
+
+
+
+
