a7c3a6102eda97e94d68a1684789078f346a75cc
aladinzecic <aladinzecic10@gmail.com>
2022-12-10 17:35:26 +0100

    push

--- 18.cas/app.js
+++ 18.cas/app.js
@@ -58,15 +58,41 @@
 
 // }
 // console.log(mala("ja sam JAAAAA"))
-function mala(recenica,indeks){
+// function mala(recenica,indeks){
     
+//     let nova=" "
+//     for(i=0;i<indeks;i++){
+//         nova+=recenica[i]
+//     }
+    
+
+// return nova
+
+// }
+// console.log(mala("ja sam JAAAAA",5))
+
+
+
+//je li recenica ana voli milovana il jok
+
+function ana(recenica){
+    let brojac=1
+    let i
     let nova=" "
-    for(i=0;i<indeks;i++){
-        nova+=recenica[i]
+    for(i=0;i<recenica.length;i++){
+        if(recenica[i]===" "){
+            continue
+        }
+        else nova+=recenica[i].toLowerCase()
     }
+    let duzina=nova.length
+    for(i=0;i<duzina/2;i++){
+        if(nova[i]===nova[duzina-brojac]){
+            brojac++
+        }
+    }
+    if(brojac===duzina/2) return "respect"
+        else return "nije"
     
-
-return nova
-
 }
-console.log(mala("ja sam JAAAAA",5))
+console.log(ana("ana VOlI milovana"))
