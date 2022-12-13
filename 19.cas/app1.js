98745adfd1fcea99a57622d02fe579968d37e873
aladinzecic <aladinzecic10@gmail.com>
2022-12-13 19:51:31 +0100

    Number

---
+++ 19.cas/app1.js
@@ -0,0 +1,46 @@
+// //JS prepoznaje samo jedan tip podataka za brojeve 
+// //U pitanju je number tip podataka i oni mogu biti zapisani sa ili bez decimalnog zapisa
+
+// //beliki br mogu biti zapisani sa tzv. exponent oznakom
+// const pet=5e6
+// console.log(pet)
+
+// const novibr=555e-5;
+// console.log(novibr)
+
+// //NaN-Not a Number
+// //not a number je js rezervisana rec koja ukazuje da broj nije korektan 
+// //NaN JE TIPA NUMBER!!!!!!!!!!!
+// //za proveru da li je neka vrednost korektan broj se koristi !isNaN()
+
+
+// console.log(11/"apple")
+// console.log(25/0)//=infinity ili -infinity
+// //infinity dobijamo i kada dobijemo broj veci od najveceg
+// console.log(0/0)
+// //hexadecimal number
+// //ako su brojevi napisani prefixom 0xffFF rezultat ce biti 255
+// const x=0xff
+// console.log(x)//255
+
+// //pored obicnog dodeljivanja vrednosti mozemo i preko new
+// const k=10
+// const l=new Number(33)
+// console.log(k+l)
+// //razlika je
+
+
+
+
+//napravi funkicu koja pretvara km u m
+// function izdvajanje1(par1){
+//     par1=par1*1000
+//     return par1
+// }
+// console.log(izdvajanje1(4))
+
+// function izdvajanje(par){
+    
+//     return par*2.54
+// }
+// console.log(izdvajanje(4))
