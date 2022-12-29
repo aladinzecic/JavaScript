3e10ca551517aa6ecddea944a71ed599354df940
aladinzecic <aladinzecic10@gmail.com>
2022-12-29 19:41:48 +0100

    push

--- 23. cas/app.js
+++ 23. cas/app.js
@@ -32,3 +32,10 @@ console.log(datum1);
 
 // Date.now() nam daje trenutno vreme u milisekundama
 //Date.parse() nam daje vreme u milisekundama od nekog datuma
+// Napraviti funkciju koja racuna razliku u danima izmedju sledeca dva datuma:
+// "1999-05-24" i "2004-09-15"
+
+function funkcija(par1, par2) {
+  return (Date.parse(par1) - Date.parse(par2)) / 86400000;
+}
+console.log(funkcija("2004 sep 15", "99 may 24"));
