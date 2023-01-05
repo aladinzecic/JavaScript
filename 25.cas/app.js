997a34e04fde01022b05c3e1ef5e647e63fe95c5
aladinzecic <aladinzecic10@gmail.com>
2023-01-05 14:11:36 +0100

    domaci js

--- 25.cas/app.js
+++ 25.cas/app.js
@@ -110,6 +110,18 @@ console.log(subota);
 // koliko tipa number,
 // koliko tipa boolean,
 // koliko elemenata je zapravo niz?
+// const nizz = [
+//   "jagoda",
+//   25,
+//   true,
+//   "mandarina",
+//   [1, 2, 3],
+//   "ananas",
+//   49,
+//   694,
+//   "tresnja",
+// ];
+
 const nizz = [
   "jagoda",
   25,
@@ -121,3 +133,20 @@ const nizz = [
   694,
   "tresnja",
 ];
+let stringg = 0;
+let numberr = 0;
+let booleann = 0;
+for (let i = 0; i <= nizz.length - 1; i++) {
+  if (typeof nizz[i] == "string") {
+    stringg++;
+  } else if (typeof nizz[i] == "number") {
+    numberr++;
+  } else if (typeof nizz[i] == "boolean") {
+    booleann++;
+  }
+}
+let duzina = nizz.length;
+console.log(
+  `ima ${stringg} stringova,${booleann} booleanna i ${numberr}numbera`
+);
+console.log(`niz ima${duzina} elemenata`);
