dccb956fd52fe043d19ea0eb2e5707db2b7e5091
aladinzecic <aladinzecic10@gmail.com>
2023-02-01 23:58:06 +0100

    js

--- 36.cas/app.js
+++ 36.cas/app.js
@@ -83,3 +83,24 @@ const novi = godina.filter(function (element) {
   return element.starost > 17;
 });
 console.log(novi);
+
+// Domaci:
+//  1. Write a JavaScript program to sort an array of JavaScript objects:
+
+const library = [
+  {
+    title: "The Road Ahead",
+    author: "Bill Gates",
+    libraryID: 1254,
+  },
+  {
+    title: "Walter Isaacson",
+    author: "Steve Jobs",
+    libraryID: 4264,
+  },
+  {
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    author: "Suzanne Collins",
+    libraryID: 3245,
+  },
+];
